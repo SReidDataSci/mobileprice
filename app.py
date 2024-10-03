242fa1235e6b27f4194969741980ec74350632d4
SReidDataSci <146045349+SReidDataSci@users.noreply.github.com>
2024-10-03 15:42:19 -0600

    minor change

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask, request, jsonify
+import numpy as np
+import pandas as pd
+from sklearn.tree import DecisionTreeClassifier
+
+app = Flask(__name__)
+
+url = "https://raw.githubusercontent.com/SReidDataSci/mobileprice/main/data/train.csv"
+dataframe = pd.read_csv(url)
+
+X = dataframe.iloc[:, :-1].values
+Y = dataframe.iloc[:, -1].values
+
+clf = DecisionTreeClassifier()
+clf.fit(X, Y)
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    # Get the phone data from the request
+    data = request.json
+    
+    # Example: ['battery_power', 'blue', 'clock_speed', etc...]
+    features = [data['battery_power'], data['blue'], data['clock_speed'], data['dual_sim'],
+                data['fc'], data['four_g'], data['int_memory'], data['m_dep'], data['mobile_wt'], 
+                data['n_cores'], data['px_height'], data['px_width'], data['ram'], data['sc_h'], 
+                data['sc_w'], data['talk_time'], data['three_g'], data['touch_screen'], data['wifi']]
+    
+    # Reshape the input for prediction
+    features = np.array(features).reshape(1, -1)
+    
+    # Make the prediction
+    prediction = clf.predict(features)
+    
+    # Return the prediction as a JSON response
+    return jsonify({'price_range': int(prediction[0])})
+
+if __name__ == '__main__':
+    app.run(debug=True)
