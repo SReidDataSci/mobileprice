a456ad864713a321515ce2dd7079f7914d8997a0
SReidDataSci <sarah.reid2@ucalgary.ca>
2024-10-03 17:07:14 -0600

    build folder

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import PhonePricePredictor from './PhonePricePredictor';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <PhonePricePredictor />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
