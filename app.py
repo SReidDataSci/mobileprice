10e20b01224eefec35e22b669cc56952e64d81fc
SReidDataSci <sarah.reid2@ucalgary.ca>
2024-10-07 16:57:16 -0600

    change to CORS

--- app.py
+++ app.py
@@ -6,7 +6,8 @@ from sklearn.tree import DecisionTreeClassifier
 
 app = Flask(__name__)
 # Allow all origins and all methods
-CORS(app, resources={r"/*": {"origins": ["http://localhost:3000", "https://sreiddatasci.github.io/portfolio/"]}}, supports_credentials=True)
+CORS(app)
+# CORS(app, resources={r"/*": {"origins": ["http://localhost:3000", "https://sreiddatasci.github.io/portfolio/,"]}}, supports_credentials=True)
 
 url = "https://raw.githubusercontent.com/SReidDataSci/mobileprice/refs/heads/main/data/train.csv"
 dataframe = pd.read_csv(url)
